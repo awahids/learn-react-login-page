15d13659ce9f76cd88d4599002848daf3ce51b86
awhds <awahid.safhadi@gmail.com>
2022-07-13 22:31:08 +0700

    refactory stucture react

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import NavBar from './components/NavBar';
 import Intro from './components/Intro';
 import Trending from './components/Trending';
 import './style/LandingPage.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>
-      <div className='myBG'>
-        <NavBar />
-        <Intro />
-      </div>
+      <Home />
+      <NavBar />
     </div>
   );
 }
