63e70de9efa60e233d115f7cedb6309d7d66f40e
awhds <awahid.safhadi@gmail.com>
2022-07-13 22:48:42 +0700

    delete react booststrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import Intro from './components/Intro';
-import Trending from './components/Trending';
-import './style/LandingPage.css';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div>
-      <Home />
+      {/* <Home /> */}
       <NavBar />
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,15 @@
-import {Navbar, Container, Nav} from 'react-bootstrap';
-
 const NavBar = () => {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href='/'>Nonton Films</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
-          <Nav.Link href="#trending">Trending</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
+    <div className="navbar">
+      <span className="logo">Learn React</span>
+      <ul className="nav-list">
+        <li className="nav-item">
+          <img src="" alt="" className="avatar" />
+        </li>
+        <li className="nav-item">John Doe</li>
+        <li className="nav-item">Logout</li>
+      </ul>
+    </div>
   );
 }
 
