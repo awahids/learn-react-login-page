e983cacfe51b7fa2d0c1edc6585e0cae97e08df5
awhds <awahid.safhadi@gmail.com>
2022-07-14 07:35:41 +0700

    add page posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
+import Post from './pages/Post';
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
+      <Post />
     </div>
   );
 }
